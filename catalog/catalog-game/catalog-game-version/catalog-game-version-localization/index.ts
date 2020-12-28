0d21f169bff5c9aabb2081fa471ccc3a39a65cd9
jameswilddev <jameswilddev@outlook.com>
2020-12-28 19:51:55 +0000

    Add more metadata.

--- catalog/catalog-game/catalog-game-version/catalog-game-version-localization/index.ts
+++ catalog/catalog-game/catalog-game-version/catalog-game-version-localization/index.ts
@@ -1,8 +1,37 @@
+import { CatalogImage } from "../../../catalog-image";
+
 export type CatalogGameVersionLocalization = {
   /**
    * The name of the game as of the version localization.
    */
   readonly gameName: string;
+
+  /**
+   * A blurb-like description of the game as of the version localization.
+   */
+  readonly gameDescription: string;
+
+  /**
+   * A square icon for the game as of the version localization.  May be cropped
+   * to a circle.
+   */
+  readonly gameIcon: CatalogImage;
+
+  /**
+   * A background for the game in browsers.  Likely to be cropped to match the
+   * display's aspect ratio.
+   */
+  readonly gameBackdrop: CatalogImage;
+
+  /**
+   * The name of the version.
+   */
+  readonly versionName: string;
+
+  /**
+   * A description of the changes in the version.
+   */
+  readonly versionDescription: string;
 
   /**
    * The code of the localization the version localization implements.
